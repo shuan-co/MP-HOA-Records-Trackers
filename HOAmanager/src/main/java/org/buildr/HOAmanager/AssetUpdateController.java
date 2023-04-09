3fa0bf749bbf8a70704abc8b703af04b0218d950
Shuan Co <78131504+shuan-co@users.noreply.github.com>
2023-04-09 21:54:54 +0800

    Big Changes

--- HOAmanager/src/main/java/org/buildr/HOAmanager/AssetUpdateController.java
+++ HOAmanager/src/main/java/org/buildr/HOAmanager/AssetUpdateController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import javax.script.*;
 
 
 @RestController
@@ -35,8 +36,28 @@ public class AssetUpdateController {
         jdbcTemplate.update("DELETE FROM assets WHERE asset_id = ? AND NOT EXISTS (SELECT 1 FROM asset_transactions WHERE asset_id = ?)", id, id);
     }
 
+    @GetMapping("/myEndpoint3")
+    public int myEndpoint3() {
+        Object result = jdbcTemplate.queryForObject("SELECT MAX(asset_id)+1 FROM assets", Object.class);
+        if (result == null){
+            return 5000;
+        }
+        else {
+            return Integer.parseInt(result.toString());
+        }
+    }
+
+    @GetMapping("/myEndpoint4")
+    public void myEndpoint4(@RequestParam int id) {
+        jdbcTemplate.update("UPDATE assets SET status = 'X' WHERE asset_id = ? AND status = 'S'",id);
+    }
+
+
     @PostMapping("/addAsset")
-    public void addAsset(@RequestBody AssetTable asset) {
+    public void addAsset(@RequestBody AssetTable asset) throws ScriptException {
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("js");
+        try 
+        {
         String update = "UPDATE assets " +
                 "SET asset_name = ?, " +
                 "    asset_description = ?, " +
@@ -49,6 +70,20 @@ public class AssetUpdateController {
                 "    loc_longiture = ?, " +
                 "    hoa_name = ? " +
                 "WHERE asset_id = ?";
-        jdbcTemplate.update(update, asset.getName(), asset.getDescription(), asset.getAcquisitionDate(), asset.isForrent(), asset.getAssetValue(), asset.getType(), asset.getStatus(), asset.getLocLattitude(), asset.getLocLongitude(), asset.getHoaName(), asset.getId());
+                jdbcTemplate.update(update, asset.getName(), asset.getDescription(), asset.getAcquisitionDate(), asset.isForrent(), asset.getAssetValue(), asset.getType(), asset.getStatus(), asset.getLocLattitude(), asset.getLocLongitude(), asset.getHoaName(), asset.getId());
+        } 
+        catch (Exception e) 
+        {
+            String[] parts = e.getMessage().split(";");
+            engine.eval("alert('" + parts[parts.length - 1] + "');");
+        }
+
+    }
+
+    @PostMapping("/insertAsset")
+    public void insertAsset(@RequestBody AssetTable asset) {
+        String query = "INSERT INTO assets (asset_id, asset_name, asset_description, acquisition_date, forrent, asset_value, type_asset, status, loc_lattitude, loc_longiture, hoa_name, enclosing_asset) "
+            + "VALUES (?, ?, ?, NOW(), ?, ?, ?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(query, asset.getId(), asset.getName(), asset.getDescription(), asset.isForrent(), asset.getAssetValue(), asset.getType(), asset.getStatus(),asset.getLocLattitude(), asset.getLocLongitude(), asset.getHoaName(), null);
     }
 }
